fbbe9efa6d2f3bfc985213b52aa67d75fb162517
nagymathev <nagymathev@gmail.com>
2023-07-19 13:02:58 +0200

    projection view model

--- opengl/src/main.cpp
+++ opengl/src/main.cpp
@@ -37,7 +37,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(960, 580, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -59,10 +59,10 @@ int main(void)
 
     {
         const float positions[] = {
-            -0.5f, -0.5f, 0.0f, 0.0f, // 0
-             0.5f, -0.5f, 1.0f, 0.0f, // 1
-             0.5f,  0.5f, 1.0f, 1.0f, // 2
-            -0.5f,  0.5f, 0.0f, 1.0f // 3
+             100.0f,  100.0f, 0.0f, 0.0f, // 0
+             200.0f,  100.0f, 1.0f, 0.0f, // 1
+             200.0f,  200.0f, 1.0f, 1.0f, // 2
+             100.0f,  200.0f, 0.0f, 1.0f // 3
         };
 
         unsigned int indices[] = {
@@ -83,12 +83,16 @@ int main(void)
 
         IndexBuffer ib(indices, 6);
 
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+        glm::mat4 mvp = proj * view * model;
 
         Shader shader("res/shaders/Basic.glsl");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.4f, 1.0f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         Texture texture("res/textures/awesomeface.png");
         texture.Bind();
